6c40710e148d1f24ea80696bf189d8dab6a03c7f
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2022-10-24 09:40:34 +0200

    New developments

--- javascript.js
+++ javascript.js
@@ -1,5 +1,19 @@
-let answer = document.getElementById("password").value;
+
 
 function firstPassword() {
-    alert(answer);
+    let answer = document.getElementById("password").value;
+    if (answer === "test") {
+        window.location.href = "second.html";
+    } else {
+        document.getElementById("message").textContent = "Incorrect. Try again...";
+    }
+}
+
+function secondPassword() {
+    let answer = document.getElementById("password").value;
+    if (answer === "test2") {
+        alert("oh yeah");
+    } else {
+        document.getElementById("message").textContent = "Incorrect. Try again...";
+    }
 }
