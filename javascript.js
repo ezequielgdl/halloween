5ea78d97bbd6a3c43de65a4d65161896855aff17
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2022-10-26 07:57:08 +0200

    Update function and CSS

--- javascript.js
+++ javascript.js
@@ -2,18 +2,38 @@
 
 function firstPassword() {
     let answer = document.getElementById("password").value;
-    if (answer === "test") {
-        window.location.href = "second.html";
-    } else {
-        document.getElementById("message").textContent = "Incorrect. Try again...";
+    let message = document.getElementById("message");
+    let riddle = document.getElementById("riddle");
+    let passwordContainer = document.getElementById("password-container");
+    let itinerary = document.getElementById("itinerary");
+    const image = document.createElement('img');
+    image.src  = '/images/IMG_1107.jpg';
+    image.id = 'image';
+    if (answer === "calendar") {
+        document.querySelector('.main-container').appendChild(image);
+        riddle.textContent = "ENTER THE NUMERIC CODE";
+        message.textContent = "";
+    } else if (answer === "3510"){  
+        alert("Very well! Now if you want to escape you need to answer some riddles");
+        document.getElementById('image').style.display="none";
+        message.textContent = ""; 
+        riddle.textContent = "Which word in the dictionary is spelled incorrectly?";
+    } else if (answer === "incorrectly") {
+        alert("Almost got you there! Let's continue...");
+        message.textContent = "";
+        riddle.textContent = "John's mother has 5 children. their names are March, April, May, June, and _______?";
+    } else if (answer === "john" || answer === "John") {
+        alert("Great... you might deserve escaping...");
+        riddle.textContent = "";
+        message.textContent = "";
+        itinerary.id = "itinerary-show";
+    } else if (answer === "death" || answer === "DEATH") {
+        alert("Lucky...");
+        message.textContent = "";
+        itinerary.id = "itinerary";
+        riddle.textContent = "There's one more riddle, but it's not here... it's in one of these places: ";
     }
-}
-
-function secondPassword() {
-    let answer = document.getElementById("password").value;
-    if (answer === "test2") {
-        alert("oh yeah");
-    } else {
-        document.getElementById("message").textContent = "Incorrect. Try again...";
+    else {
+        message.textContent = "Incorrect. Try again...";
     }
 }
